32b500ca4e1be63f52ba6fda73c6abeafdf75b53
Lurenjia-PasserbyA <3509057421@qq.com>
2026-08-25 16:16:09 +0800

    Create flow

--- src/main/java/org/passerbya/CommandManager.java
+++ src/main/java/org/passerbya/CommandManager.java
@@ -1,7 +1,25 @@
 package org.passerbya;
 
-public class CommandManager {
-    public static void main(String[] args) {
+import java.io.*;
+import java.lang.ProcessBuilder;
 
+public class CommandManager {
+
+    public static void main(String[] args) {
+        try {
+            ProcessBuilder pd = new ProcessBuilder("powershell.exe");
+            Process process = pd.start();
+
+            Reader OutputReader = new InputStreamReader(process.getInputStream());
+            BufferedReader stdout = new BufferedReader(OutputReader);
+
+            OutputStream stdin = process.getOutputStream();
+
+            Reader ErrorReader = new InputStreamReader(process.getErrorStream());
+            BufferedReader stderr =new BufferedReader(ErrorReader);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
